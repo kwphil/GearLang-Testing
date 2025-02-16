ce9e512adde1545b10be3294fbb148c97aa5c1ac
korb <phillipsw1980@gmail.com>
2025-02-15 20:13:36 -0600

    Basic token format

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+// This file is part of the GearLang v0.0.1 executable. This library is free
+// software; you can redistribute it and/or modify it under the terms
+// of the GNU General Public License as published by the Free Software
+// Foundation; either version 3, or (at your option) any later version.
+
+// This executable is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details. 
+
+// Under Section 7 of GPL version 3, you are granted additional
+// permissions described in the GCC Runtime Library Exception, version
+// 3.1, as published by the Free Software Foundation.
+
+// You should have received a copy of the GNU General Public License and
+// a copy of the GCC Runtime Library Exception along with this program;
+// see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
+// <http://www.gnu.org/licenses/>.
+
+#include <iostream>
+#include <span>
+
+int main(int argc, char *argv[]) {
+    std::string filename = argv[1];
+
+    // First need to start by tokenizing the input
+    
+    
+    return EXIT_SUCCESS;
+}
