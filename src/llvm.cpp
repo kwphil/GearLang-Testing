34c54387e9d4a9e4a7a974878ff95b06c7f7867c
korb <phillipsw1980@gmail.com>
2025-03-01 12:43:10 -0600

    Switched from GPLv3 to MIT No Attribution License

--- src/llvm.cpp
+++ src/llvm.cpp
@@ -1,3 +1,12 @@
+// <gearlang/llvm.cpp> -*- C++ -*-
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Function.h>
--- src/tree/builder.cpp
+++ src/tree/builder.cpp
@@ -1,23 +1,11 @@
 // <gearlang/tree/builder.cpp> -*- C++ -*-
 //
-// This file is part of the GearLang v0.0.1 executable. This executable is free
-// software; you can redistribute it and/or modify it under the
-// terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 3, or (at your option)
-// any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// Under Section 7 of GPL version 3, you are granted additional
-// permissions described in the GCC Runtime Library Exception, version
-// 3.1, as published by the Free Software Foundation.
-//
-// You should have received a copy of the GNU General Public License and
-// a copy of the GCC Runtime Library Exception along with this program;
-// If not, see <http://www.gnu.org/licenses/>.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 
 #pragma once
 
